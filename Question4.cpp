3d4cf11dfc0cdc6c1a979cc6013734fd57876e91
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-08-24 21:22:09 +0530

    Create Question4.cpp

---
+++ Question4.cpp
@@ -0,0 +1,17 @@
+//Question 4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c,d;
+cout << "Enter length: ";
+cin  >> a;
+cout << "Enter breadth: ";
+cin >> b;
+c= 2*a+2*b
+cout << "Perimeter of rectangle: " << c;
+d= a*b;
+cout << "Area of rectangle: " << d;
+return 0;
+} 
