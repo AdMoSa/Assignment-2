4418637b2f7cfe81544938c16df3b40168b0425e
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-08-29 16:17:01 +0530

    Create 2nd ques7.cpp

---
+++ 2nd ques7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a,b,c;
+cout << "Enter the two angles of the triangles: ";
+cin >> a;
+cin >> b;
+c= 180-(a+b);
+cout << "Third angle of the triangle: " << c;
+}
