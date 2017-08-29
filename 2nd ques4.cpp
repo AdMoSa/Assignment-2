3ddbc109b21ea01e2b315d6c23bcf761dc4b8717
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-08-29 16:09:06 +0530

    Create 2nd ques4.cpp

---
+++ 2nd ques4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout << "Enter no of days: ";
+cin >> a;
+b= a/7,c= a/365;
+cout << "No. of weeks: " << b;
+cout << "No. of years: " << c;
+cout << "No. of days: " <<a;
+return 0;
+}
