1ebb9a7a8859e1122902cc87cb5b660eb14ecd85
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-08-29 16:10:14 +0530

    Create 2nd ques5.cpp

---
+++ 2nd ques5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+float a,b,c;
+cout << "Enter no.: ";
+cin >> a;
+cout << "To be raised to power: ";
+cin >> b;
+c= pow(a,b);
+cout << "a^b: " << c;
+}
