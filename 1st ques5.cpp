d0042344a5e23b8911efb075ae63c49aac9bd105
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-08-29 17:21:02 +0530

    Create 1st ques5.cpp

---
+++ 1st ques5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main()
+{
+int a,b;
+cout << "Enter length: ";
+cin >> a;
+cout << "Enter breadth: ";
+cin >> b;
+cout << "Area: " << a << "*" << b << "=" << a*b;
+}
