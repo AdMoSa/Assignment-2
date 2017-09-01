bb892a72699523657426be3029201ca7bfa81653
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-01 13:38:30 +0530

    Update 1st ques1.cpp

--- 1st ques1.cpp
+++ 1st ques1.cpp
@@ -1,14 +1,29 @@
 # chufury
 //Question 1
-#include <iostream>
-using namespace std
-
-int main()
+#include<iostream>
+using namespace std;
+main()
 {
-bool a;
-cout << "Enter a Boolean no: ";
-cin >> a;
-cout << a;
+int a;
+float b;
+char c;
+double d;
+bool e;
+cout<<"Enter a integer : ";
+cin>>a;
+cout<<"Enter a float : ";
+cin>>b;
+cout<<"Enter a char : ";
+cin>>c;
+cout<<"Enter a double : ";
+cin>>d;
+cout<<"Enter a bool : ";
+cin>>e;
+cout<<"interger : "<<a;
+cout<<"float : "<<b;
+cout<<"char : "<<c;
+cout<<"double : "<<d;
+cout<<"bool : "<<e;
 }
 
 //Question 1 over
